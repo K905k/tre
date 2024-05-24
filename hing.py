027c691908117b0d61155947d8290f038483ed41
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-24 03:11:14 +0300

    Update hing.py

--- hing.py
+++ hing.py
@@ -3,6 +3,7 @@ from time import sleep
 from random import choice
 from base64 import b64decode
     from requests import get
+    from requests import get
     from telethon import TelegramClient, sync, errors, functions, types
     from telethon.tl.functions.account import CheckUsernameRequest, UpdateUsernameRequest
     from telethon.tl.functions.channels import JoinChannelRequest
